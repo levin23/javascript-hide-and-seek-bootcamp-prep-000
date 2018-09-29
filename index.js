123ac1cb3b13d74af880fc4b25889fe8750d22de
Levi C Uzodike <leviuzodike@gmail.com>
2018-09-29 16:56:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector("#nested .target")
+}
+
+function increaseRankBy(n){
+  var lis=document.querySelectorAll(".ranked-list li")
+  for(let i=0; i<lis.length; i++){
+    lis[i].innerHTML=parseInt(lis[i].innerHTML)+n
+  }
+}
+
+function deepestChild(){
+  var lis=document.querySelectorAll("#grand-node div")
+  //return lis.length
+  for(let i=0; i<lis.length; i++){
+    if(lis[i].children.length==0)return lis[i]
+  }
+}
